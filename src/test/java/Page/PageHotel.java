20aaa8ea850c27df8f4dc89bcb825b2f715a1ba2
veronica.velez <mavevelez9@gmail.com>
2023-05-05 12:31:39 -0300

    Se agregan carpetas

--- src/test/java/Page/PageHotel.java
+++ src/test/java/Page/PageHotel.java
@@ -7,16 +7,64 @@ import org.openqa.selenium.WebDriver;
 public class PageHotel extends SeleniumBase {
     public PageHotel(WebDriver driver) {
         super(driver);
+    }
 
-        By btnhoteles1 = By.xpath("//div[@class='d-1ytebqy e10w470p3']//a[.='Hoteles']");
-        By btnBuscarAlojamiento = By.xpath("//*[@id=\":R8qjalalaqlql2m:\"]");
-        By FechaDeEntrada = By.xpath("//button[@class='d-i9zc7t']");
-        By FechaDeSalida = By.xpath("//button[@class='d-i9zc7t']");
-        By PersonasHabitaciones = By.xpath("//button[@class='d-1rd1vwn']");
-        By btnBuscar = By.xpath("//button[@class='d-1jmk4ql']");
-        By btnEstrllas = By.xpath("//div[@id='Pill-StarsContainer']");
-        By btnAplicar = By.xpath("//div[@class='sc-eZYOHW khSA-dJ']");
+    By btnHotel = By.xpath("//div[@class='d-1ytebqy e10w470p3']//a[.='Hoteles']");
+    By btnBuscarAlojamiento = By.xpath("//*[@id=\":R8qjalalaqlql2m:\"]");
+    By FechaDeEntrada = By.xpath("//button[@aria-label='Fecha de entrada' and span/text()='lun, 8 may']");
+    By FechaDeSalida = By.xpath("//button[@aria-label='Fecha de salida' and span/text()='dom, 28 may']");
+    By PersonasHabitaciones = By.xpath("//button[@class='d-1rd1vwn']");
+    By btnBuscar = By.xpath("//button[@class='d-1jmk4ql']");
+    By btnEstrllas = By.xpath("//div[@id='Pill-StarsContainer']");
+    By btnAplicar = By.xpath("//div[@class='sc-eZYOHW khSA-dJ']");
 
+
+    public void Seleccionar_btnHotel () {
+
+        clickear(btnHotel);
 
     }
+
+
+    public void BuscarAlojamieto (){
+        escribir("Roma, Italia", btnBuscarAlojamiento);
+    }
+    public void SeleccionaFechaDeEntrada () {
+        clickear(FechaDeEntrada);
+
+    }
+
+    public void SeleccionarFechaDeSalida () {
+        clickear(FechaDeSalida);
+    }
+
+    public void SeleccionarPersonasHabitaciones () {
+        clickear(PersonasHabitaciones);
+    }
+
+    public void SeleccionarbtnBuscar () {
+        clickear(btnBuscar);
+    }
+
+    public void SeleccionarbtnEstrllas () {
+        clickear(btnEstrllas);
+    }
+
+    public void SeleccionarbtnAplicar () {
+        clickear(btnAplicar);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
